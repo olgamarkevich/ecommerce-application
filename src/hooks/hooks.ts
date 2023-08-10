1a2d4a03ce167a24e612a86ab2116f2a464b2f42
AleksGF <aleksgf@mail.zp.ua>
2023-08-10 16:56:08 +0300

    feat: add hook for customer login

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -4,8 +4,10 @@ import {
   type TypedUseSelectorHook,
 } from 'react-redux';
 import type { RootState, RootDispatch } from '../store/store';
+import { useGetCustomerTokenQuery } from '../api/authApi';
 
 export const useAppDispatch = () => {
   return useDispatch<RootDispatch>();
 };
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useCustomerLogin = useGetCustomerTokenQuery;
