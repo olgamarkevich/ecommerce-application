73e2d678d57a6bda6fa3bb15ff58ff04692f0c7a
AleksGF <aleksgf@mail.zp.ua>
2023-08-10 18:11:01 +0300

    feat: add logout button to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { type MouseEventHandler } from 'react';
 import type { FC } from 'react';
-import { NavLink } from 'react-router-dom';
-
+import { NavLink, useNavigate } from 'react-router-dom';
 import style from './Header.module.css';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { logoutCustomer } from '../../store/authSlice';
 
 const Header: FC = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const { userType } = useAppSelector((state) => {
+    return state.auth;
+  });
+
+  const logoutHandler: MouseEventHandler<HTMLAnchorElement> = async (e) => {
+    e.preventDefault();
+    await dispatch(logoutCustomer());
+    navigate('/');
+  };
+
   return (
     <header className={style.header}>
       <div className={style.logo}>
         {' '}
         <NavLink to='/'>Logo</NavLink>
       </div>
-      <div className={style.haeder__left}>
+      <div className={style.header__left}>
         <nav className={style.nav}>
           <ul>
-            <li className={style.nav__link}>
-              <NavLink to='/login'>log in</NavLink>
-            </li>
-            <li className={style.nav__link}>
-              <NavLink to='/signup'>sigh up</NavLink>
-            </li>
+            {userType !== 'customer' && (
+              <li className={style.nav__link}>
+                <NavLink to='/login'>log in</NavLink>
+              </li>
+            )}
+            {userType !== 'customer' && (
+              <li className={style.nav__link}>
+                <NavLink to='/signup'>sigh up</NavLink>
+              </li>
+            )}
+            {userType === 'customer' && (
+              <li className={style.nav__link}>
+                <NavLink to='/' onClick={logoutHandler}>
+                  Log out
+                </NavLink>
+              </li>
+            )}
           </ul>
         </nav>
       </div>
