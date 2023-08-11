acda9592308ac81b6dcd5e9492cb1c9e97dd45c6
AleksGF <aleksgf@mail.zp.ua>
2023-08-11 19:17:24 +0300

    feat: add useCustomerAuthorization hook

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -5,9 +5,47 @@ import {
 } from 'react-redux';
 import type { RootState, RootDispatch } from '../store/store';
 import { useGetCustomerTokenQuery } from '../api/authApi';
+import type { getCustomerTokenResponse } from '../types/apiTypes';
+import {
+  getCustomerIdFromScopes,
+  saveCustomerToLocalStorage,
+} from '../helpers/appHelpers';
+import { setUserAuthorization } from '../store/authSlice';
 
 export const useAppDispatch = () => {
   return useDispatch<RootDispatch>();
 };
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useCustomerAuthorization = (
+  data: getCustomerTokenResponse | undefined,
+): { isApplied: boolean } => {
+  const dispatch = useAppDispatch();
+
+  if (
+    data &&
+    typeof data === 'object' &&
+    'access_token' in data &&
+    'refresh_token' in data &&
+    'scope' in data
+  ) {
+    const customer = getCustomerIdFromScopes(data.scope);
+
+    if (customer) {
+      const customerData = {
+        ...customer,
+        accessToken: data.access_token,
+        refreshToken: data.refresh_token,
+      };
+
+      dispatch(setUserAuthorization(customerData));
+      saveCustomerToLocalStorage(customerData);
+
+      return { isApplied: true };
+    }
+  }
+
+  return { isApplied: false };
+};
+
 export const useCustomerLogin = useGetCustomerTokenQuery;
