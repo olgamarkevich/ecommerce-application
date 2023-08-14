dd66ae9753d3bed06dfcb59758e2830f69f44935
AleksGF <aleksgf@mail.zp.ua>
2023-08-14 10:14:58 +0300

    feat: add layout for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header } from 'components';
+import { Routes, Route } from 'react-router-dom';
 import { Login, Main, Page404, SignUp } from 'pages';
+import Layout from './components/Layout/Layout';
 import { useInit } from './hooks/hooks';
+import './App.css';
 
 function App() {
   useInit();
 
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <Header />
-        <div className='wrapper'>
-          <Routes>
-            <Route path='/' index element={<Main />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='*' element={<Page404 />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Main />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='*' element={<Page404 />} />
+      </Route>
+    </Routes>
   );
 }
 
