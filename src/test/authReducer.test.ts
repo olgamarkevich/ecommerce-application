c16c145a64914b2528e4020e0c326591ed9cb1f8
AleksGF <aleksgf@mail.zp.ua>
2023-08-14 13:53:40 +0300

    feat: add test description

--- src/test/authReducer.test.ts
+++ src/test/authReducer.test.ts
@@ -4,74 +4,76 @@ import reducer, {
 } from '../store/authSlice';
 import type { AuthState } from '../types/storeTypes';
 
-test('should return the initial state', () => {
-  expect(reducer(undefined, { type: undefined })).toEqual({
-    userType: null,
-    customerId: null,
-    accessToken: '',
-    refreshToken: '',
+describe('authReducer should work correctly', () => {
+  test('should return the initial state', () => {
+    expect(reducer(undefined, { type: undefined })).toEqual({
+      userType: null,
+      customerId: null,
+      accessToken: '',
+      refreshToken: '',
+    });
   });
-});
 
-test('should set new User', () => {
-  const previousState: AuthState = {
-    userType: null,
-    customerId: null,
-    accessToken: '',
-    refreshToken: '',
-  };
+  test('should set new User', () => {
+    const previousState: AuthState = {
+      userType: null,
+      customerId: null,
+      accessToken: '',
+      refreshToken: '',
+    };
 
-  const newUser: Omit<AuthState, 'isDataLoaded'> = {
-    userType: 'anonymous',
-    customerId: 'someinterestingstringwithcustomerid',
-    accessToken: 'token',
-    refreshToken: 'refresh token',
-  };
+    const newUser: AuthState = {
+      userType: 'anonymous',
+      customerId: 'someinterestingstringwithcustomerid',
+      accessToken: 'token',
+      refreshToken: 'refresh token',
+    };
 
-  expect(reducer(previousState, setUserAuthorization(newUser))).toEqual({
-    userType: 'anonymous',
-    customerId: 'someinterestingstringwithcustomerid',
-    accessToken: 'token',
-    refreshToken: 'refresh token',
+    expect(reducer(previousState, setUserAuthorization(newUser))).toEqual({
+      userType: 'anonymous',
+      customerId: 'someinterestingstringwithcustomerid',
+      accessToken: 'token',
+      refreshToken: 'refresh token',
+    });
   });
-});
 
-test('should change User', () => {
-  const previousState: AuthState = {
-    userType: 'customer',
-    customerId: 'somenotinterestingcustomerid',
-    accessToken: 'token',
-    refreshToken: 'refresh token',
-  };
+  test('should change User', () => {
+    const previousState: AuthState = {
+      userType: 'customer',
+      customerId: 'somenotinterestingcustomerid',
+      accessToken: 'token',
+      refreshToken: 'refresh token',
+    };
 
-  const newUser: Omit<AuthState, 'isDataLoaded'> = {
-    userType: 'anonymous',
-    customerId: 'someinterestingstringwithcustomerid',
-    accessToken: 'new token',
-    refreshToken: 'new refresh token',
-  };
+    const newUser: AuthState = {
+      userType: 'anonymous',
+      customerId: 'someinterestingstringwithcustomerid',
+      accessToken: 'new token',
+      refreshToken: 'new refresh token',
+    };
 
-  expect(reducer(previousState, setUserAuthorization(newUser))).toEqual({
-    userType: 'anonymous',
-    customerId: 'someinterestingstringwithcustomerid',
-    accessToken: 'new token',
-    refreshToken: 'new refresh token',
+    expect(reducer(previousState, setUserAuthorization(newUser))).toEqual({
+      userType: 'anonymous',
+      customerId: 'someinterestingstringwithcustomerid',
+      accessToken: 'new token',
+      refreshToken: 'new refresh token',
+    });
   });
-});
 
-test('should remove User data with removeUserAuthorization', () => {
-  const previousState: AuthState = {
-    userType: 'customer',
-    customerId: 'somenotinterestingcustomerid',
-    accessToken: 'token',
-    refreshToken: 'refresh token',
-  };
+  test('should remove User data with removeUserAuthorization', () => {
+    const previousState: AuthState = {
+      userType: 'customer',
+      customerId: 'somenotinterestingcustomerid',
+      accessToken: 'token',
+      refreshToken: 'refresh token',
+    };
 
-  expect(reducer(previousState, removeUserAuthorization())).toEqual({
-    userType: null,
-    customerId: null,
-    accessToken: '',
-    refreshToken: '',
+    expect(reducer(previousState, removeUserAuthorization())).toEqual({
+      userType: null,
+      customerId: null,
+      accessToken: '',
+      refreshToken: '',
+    });
   });
 });
 
