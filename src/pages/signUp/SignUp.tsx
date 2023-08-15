b02bf439610da4452adf04314c61eddbe67497ac
Olga Markevich <pyrkoolga@gmail.com>
2023-08-15 17:30:16 +0300

    refactor: settingSchema

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -4,102 +4,40 @@ import { useForm } from 'react-hook-form';
 import style from './SignUp.module.css';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-
 import type { RequiredKeepUndefined } from 'helpers/typesHelpers';
 import SignUpInput from './SignUpInput/SignUpInput';
-
-interface CountryZipData {
-  countryName: string;
-  countryCode: string;
-  zipRegExp: RegExp;
-}
-
-const countryZipItems: CountryZipData[] = [
-  {
-    countryName: 'United States',
-    countryCode: 'US',
-    zipRegExp: /^\d{5}(?:-?\d{4})?$/,
-  },
-  {
-    countryName: 'Germany',
-    countryCode: 'DE',
-    zipRegExp: /^\d{5}$/,
-  },
-];
-
-const countryCodes = countryZipItems.map((item) => {
-  return item.countryCode;
-});
+import {
+  city,
+  cityBilling,
+  country,
+  countryBilling,
+  countryZipItems,
+  dateOfBirth,
+  email,
+  firstname,
+  lastname,
+  password,
+  postalCode,
+  postalCodeBilling,
+  street,
+  streetBilling,
+} from 'helpers/settingSchema';
 
 const schema = yup
   .object({
-    email: yup.string().email().required(),
-    password: yup
-      .string()
-      .required()
-      .trim('password cannot include leading and trailing spaces')
-      .matches(/^(?=.*[a-z])/, 'must Contain One Lowercase Character')
-      .matches(/^(?=.*[A-Z])/, 'must Contain One Uppercase Character')
-      .matches(/^(?=.*[0-9])/, 'must Contain One Number Character')
-      .min(8),
-    firstname: yup
-      .string()
-      .required()
-      .matches(/^[a-zа-яё\s-]+$/i, 'only alphabets are allowed for this field ')
-      .min(1),
-    lastname: yup
-      .string()
-      .required()
-      .matches(/^[a-zа-яё\s-]+$/i, 'only alphabets are allowed for this field ')
-      .min(1),
-    dateOfBirth: yup
-      .date()
-      .nullable()
-      .typeError('date of birth is required')
-      .max(
-        new Date(Date.now() - 18 * 365 * 24 * 60 * 60 * 1000),
-        'you must be at least 18 years',
-      ),
-    street: yup.string().required().min(1),
-    city: yup
-      .string()
-      .required()
-      .matches(/^[a-zа-яё\s-]+$/i, 'only alphabets are allowed for this field ')
-      .min(1),
-    postalCode: yup
-      .string()
-      .when('country', ([country], schema) => {
-        const zipRegExp =
-          countryZipItems.find((item) => {
-            return item.countryCode === country;
-          })?.zipRegExp || new RegExp(/.*/);
-
-        return schema.matches(zipRegExp, `invalid postalCode for ${country}`);
-      })
-      .required(),
-    country: yup
-      .string()
-      .oneOf(countryCodes, 'country is a required field')
-      .required()
-      .min(1),
-    cityBilling: yup
-      .string()
-      .matches(/^[a-zа-яё\s-]*$/, 'only alphabets are allowed for this field '),
-    streetBilling: yup.string(),
-    countryBilling: yup.string(),
-    postalCodeBilling: yup
-      .string()
-      .when('countryBilling', ([countryBilling], schema) => {
-        const zipRegExp =
-          countryZipItems.find((item) => {
-            return item.countryCode === countryBilling;
-          })?.zipRegExp || new RegExp(/.*/);
-
-        return schema.matches(
-          zipRegExp,
-          `invalid postalCode for ${countryBilling}`,
-        );
-      }),
+    ...email,
+    ...password,
+    ...firstname,
+    ...lastname,
+    ...dateOfBirth,
+    ...street,
+    ...city,
+    ...postalCode,
+    ...country,
+    ...cityBilling,
+    ...streetBilling,
+    ...countryBilling,
+    ...postalCodeBilling,
   })
   .required();
 
