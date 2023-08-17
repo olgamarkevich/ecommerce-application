eb55c2dcc269597f83ec33f763ce2b21b8de4b55
0DiNEC <as.odinson@gmail.com>
2023-08-17 17:23:09 +0300

    feat: add shadow animation

--- src/components/LinkItem/LinkItem.tsx
+++ src/components/LinkItem/LinkItem.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import type { FC } from 'react';
-import { NavLink, useMatch } from 'react-router-dom';
 import type { ILinkItem } from 'types/componentTypes';
+
+import { NavLink, useMatch } from 'react-router-dom';
 
 const LinkItem: FC<ILinkItem> = ({ to, children }) => {
   const isActive = useMatch(to);
   return (
     <NavLink
       to={to}
-      className={`flex justify-center items-center 
+      className={`relative flex justify-center items-center 
       min-w-[100px] h-[30px] p-2
       font-medium text-blue-900 
       border-solid border rounded border-blue-950
       transition duration-300 ease-in-out
+      hover:shadow-md transform hover:-translate-y-1
       ${
         isActive
           ? 'bg-blue-800 text-sky-100 hover:bg-blue-900'
