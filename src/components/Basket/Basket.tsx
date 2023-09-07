501acbabd3cfc50d326049a9fee67407837d4452
0DiNEC <as.odinson@gmail.com>
2023-09-07 17:19:40 +0300

    feat: add state itemCount

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { FC } from 'react';
 import { ReactComponent as BasketSVG } from '../../assets/svg/basket.svg';
 
 const Basket: FC = () => {
+  const [itemCount] = useState(0);
   return (
     <div>
+      {itemCount > 0 && (
+        <div className='absolute -top-2 -right-2 z-10 w-5 h-5 rounded-full bg-red-500 text-sky-50 font-f-open-sans text-sm'>
+          {itemCount}
+        </div>
+      )}
       <BasketSVG />
     </div>
   );
