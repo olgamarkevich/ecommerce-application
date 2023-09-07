3cd0bd1480ee2d3a19b6bf111e802a55b5b49260
AleksGF <aleksgf@mail.zp.ua>
2023-09-07 16:55:52 +0300

    fix: remove console.log from useCart hook

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -88,7 +88,7 @@ const Card: FC<ICard> = ({ product, imgSrcList, price, discountedPrice }) => {
           </div>
         </div>
       </Link>
-      <div className={'m-auto'}>
+      <div className={'mx-auto'}>
         <CartButton sku={product.masterVariant?.sku || ''} />
       </div>
     </div>
