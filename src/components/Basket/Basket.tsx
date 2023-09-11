a07f519613da264e0b17dde1df1dbbff9ec54e87
AleksGF <aleksgf@mail.zp.ua>
2023-09-11 17:07:52 +0300

    feat: add totalProductsQuantity from state to Basket component

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import type { FC } from 'react';
 import { ReactComponent as BasketSVG } from '../../assets/svg/basket.svg';
+import { useAppSelector } from '../../hooks/hooks';
 
 const Basket: FC = () => {
-  const [itemCount] = useState(0);
+  const { totalProductsQuantity } = useAppSelector((state) => {
+    return state.cart;
+  });
+
   return (
     <div>
-      {itemCount > 0 && (
+      {!!totalProductsQuantity && totalProductsQuantity > 0 && (
         <div className='absolute -top-2 -right-2 z-10 w-5 h-5 rounded-full bg-red-500 text-sky-50 font-f-open-sans text-sm'>
-          {itemCount}
+          {totalProductsQuantity}
         </div>
       )}
       <BasketSVG />
