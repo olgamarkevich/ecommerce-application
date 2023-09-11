1d1d65fad67de2c5030100173a52604799ffe5a2
0DiNEC <as.odinson@gmail.com>
2023-09-11 11:41:30 +0300

    feat: add hover effect to logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -10,7 +10,7 @@ const LogoLink: FC<Pick<ILinkItem, 'to'>> = ({ to }) => {
       to={to}
       className={`flex justify-center items-end
       bg-gradient-to-r from-blue-800 to-c-pink-red
-      bg-clip-text text-transparent text-xl`}
+      bg-clip-text text-transparent text-xl hover:scale-110 transition-transform duration-300`}
     >
       <Logo />
       <span className='font-bold text-2xl tracking-widest'>ECO</span>mmerce
